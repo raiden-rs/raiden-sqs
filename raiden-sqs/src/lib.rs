7317f8c7bfe6ecb67cd92e11afe08ff802a10f93
Yuki Kodama <endflow.net@gmail.com>
2020-07-27 02:08:11 +0900

    Simplify use declarations

--- raiden-sqs/src/lib.rs
+++ raiden-sqs/src/lib.rs
@@ -2,15 +2,11 @@ pub use raiden_sqs_derive::*;
 pub use rusoto_credential::*;
 
 #[cfg(feature = "default")]
-use rusoto_core_default as rusoto_core;
+pub use rusoto_core_default::*;
 #[cfg(feature = "rustls")]
-use rusoto_core_rustls as rusoto_core;
-
-pub use rusoto_core::*;
+pub use rusoto_core_rustls::*;
 
 #[cfg(feature = "default")]
-use rusoto_sqs_default as rusoto_sqs;
+pub use rusoto_sqs_default::*;
 #[cfg(feature = "rustls")]
-use rusoto_sqs_rustls as rusoto_sqs;
-
-pub use rusoto_sqs::*;
+pub use rusoto_sqs_rustls::*;
